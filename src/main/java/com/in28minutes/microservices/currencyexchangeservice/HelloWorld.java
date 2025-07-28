c2a09d2c6fbea5603ac13e7443e19d9ad175c154
Srikant <psrikant54@gmail.com>
2025-07-28 17:27:51 +0530

    second commit

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public String sayHello(String name) {
 
         System.out.println("sayHello called with name : " + name);
-        return "Hello " + name;
+        return "Hello: " + name;
     }
 
 }
